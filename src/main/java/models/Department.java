a09571d94e371b60aa4aacb79f34485d68d30a30
naisenyamungai <naisenyamungai@gmail.com>
2019-05-25 17:00:31 +0300

    create department class and all its properties and methods

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,3 +1,41 @@
 package models;
 
 import java.util.ArrayList;
+
+public class Department {
+
+    private String departmentName;
+    private String departmentAbbreviation;
+    private String divisionName;
+    private static ArrayList<Department> mInstances = new ArrayList<>();
+
+    public Department (String departmentAbbreviation, String departmentName, String divisionName){
+        this.departmentName = departmentName;
+        this.departmentAbbreviation = departmentAbbreviation;
+        this.divisionName = divisionName;
+        mInstances.add(this);
+    }
+
+    public String getName(){
+        return departmentName;
+    }
+
+    public String getAbb(){ return departmentAbbreviation;}
+
+    public String getDivision(){ return divisionName;}
+
+
+    public static ArrayList<Department> getAll(){
+        return mInstances;
+    }
+
+
+
+    public static void clearAllDepartments(){
+        mInstances.clear();
+    }
+
+
+
+}
+
