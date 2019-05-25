ecb7c10292533a7a23aaf2a9d4b33f594584221b
naisenyamungai <naisenyamungai@gmail.com>
2019-05-25 17:04:18 +0300

    create and run test on whether objects are created correctly

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,11 @@ public class DepartmentTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void NewDepartmentObjectGetsCorrectlyCreated_true() throws Exception{
+        Department department = new Department("NPD", "Network Planning and Design", "Technology");
+        assertEquals(true, department instanceof Department);
     }
 }
