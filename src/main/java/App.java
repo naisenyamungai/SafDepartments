7f0444013790f7e44a804bd9d7171413b38b9a3e
naisenyamungai <naisenyamungai@gmail.com>
2019-05-25 17:28:33 +0300

    add logic to departmentCreated.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,17 +15,6 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        ProcessBuilder process = new ProcessBuilder();
-        Integer port;
-        if (process.environment().get("PORT") != null) {
-            port = Integer.parseInt(process.environment().get("PORT"));
-        } else {
-            port = 4567;
-        }
-
-        setPort(port);
-
-
 
         get("/", (request, response) -> {
                     Map<String, Object> model = new HashMap<>();
